c54a413129de265516d83b6a876435c6f0908c95
mihaelfucek-dc <mihael.fucek@doofuscorporation.com>
2022-08-18 10:42:14 +0200

    followings added

--- public/js/common.js
+++ public/js/common.js
@@ -70,7 +70,13 @@ $("#deletePostButton").click((event) => {
     $.ajax({
         url: `/api/posts/${postId}`,
         type: "DELETE",
-        success: () => {
+        success: (data, status, xhr) => {
+
+            if(xhr.status != 202) {
+                alert("could not delete post");
+                return;
+            }
+            
             location.reload();
         }
     })
@@ -132,6 +138,41 @@ $(document).on("click", ".post", (event) => {
     if(postId !== undefined && !element.is("button")) {
         window.location.href = '/posts/' + postId;
     }
+});
+
+$(document).on("click", ".followButton", (e) => {
+    var button = $(e.target);
+    var userId = button.data().user;
+    
+    $.ajax({
+        url: `/api/users/${userId}/follow`,
+        type: "PUT",
+        success: (data, status, xhr) => { 
+            
+            if (xhr.status == 404) {
+                alert("user not found");
+                return;
+            }
+            
+            var difference = 1;
+            if(data.following && data.following.includes(userId)) {
+                button.addClass("following");
+                button.text("Following");
+            }
+            else {
+                button.removeClass("following");
+                button.text("Follow");
+                difference = -1;
+            }
+            
+            var followersLabel = $("#followersValue");
+            if(followersLabel.length != 0) {
+                var followersText = followersLabel.text();
+                followersText = parseInt(followersText);
+                followersLabel.text(followersText + difference);
+            }
+        }
+    })
 });
 
 function getPostIdFromElement(element) {
